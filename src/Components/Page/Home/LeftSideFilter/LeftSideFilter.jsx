caacd0b88a022a99b6ea7ca246f22d62e6875e9c
MehediHasan128 <mehedihasan12926@gmail.com>
2024-01-14 17:02:33 +0600

    add some section

--- src/Components/Page/Home/LeftSideFilter/LeftSideFilter.jsx
+++ src/Components/Page/Home/LeftSideFilter/LeftSideFilter.jsx
@@ -1,4 +1,4 @@
-import { Box, Slider, Typography } from "@mui/material";
+import { Box, FormControlLabel, Radio, Slider, Typography } from "@mui/material";
 import { useState } from "react";
 
 // React Icon
@@ -110,34 +110,143 @@ const LeftSideFilter = () => {
 
         {/* Departue Box */}
         <Box className="mt-5 flex flex-wrap gap-5">
-            <div className="text-center border-2 border-black w-fit p-5 rounded-lg shadow-lg">
-                <BsSunrise className="text-3xl w-fit mx-auto" />
-                <Typography variant="body1">
-                    <p className="font-semibold">Morning</p>
-                </Typography>
-                <Typography variant="body2">
-                    <p>06:00-11:59 (5)</p>
-                </Typography>
-            </div>
-            <div className="text-center border-2 border-black w-fit p-5 rounded-lg shadow-lg">
-                <MdOutlineWbSunny className="text-3xl w-fit mx-auto" />
-                <Typography variant="body1">
-                    <p className="font-semibold">After Noon</p>
-                </Typography>
-                <Typography variant="body2">
-                    <p>12:00-17:59 (5)</p>
-                </Typography>
-            </div>
-            <div className="text-center border-2 border-black w-fit p-5 rounded-lg shadow-lg">
-                <BsSunset className="text-3xl w-fit mx-auto" />
-                <Typography variant="body1">
-                    <p className="font-semibold">Evening</p>
-                </Typography>
-                <Typography variant="body2">
-                    <p>18:00-23:59 (7)</p>
-                </Typography>
-            </div>
+          <div className="text-center border-2 border-black w-fit p-5 rounded-lg shadow-lg">
+            <BsSunrise className="text-3xl w-fit mx-auto" />
+            <Typography variant="body1">
+              <p className="font-semibold">Morning</p>
+            </Typography>
+            <Typography variant="body2">
+              <p>06:00-11:59 (5)</p>
+            </Typography>
+          </div>
+          <div className="text-center border-2 border-black w-fit p-5 rounded-lg shadow-lg">
+            <MdOutlineWbSunny className="text-3xl w-fit mx-auto" />
+            <Typography variant="body1">
+              <p className="font-semibold">After Noon</p>
+            </Typography>
+            <Typography variant="body2">
+              <p>12:00-17:59 (5)</p>
+            </Typography>
+          </div>
+          <div className="text-center border-2 border-black w-fit p-5 rounded-lg shadow-lg">
+            <BsSunset className="text-3xl w-fit mx-auto" />
+            <Typography variant="body1">
+              <p className="font-semibold">Evening</p>
+            </Typography>
+            <Typography variant="body2">
+              <p>18:00-23:59 (7)</p>
+            </Typography>
+          </div>
         </Box>
+      </Box>
+
+      <div className="my-5 border">
+        <hr />
+      </div>
+
+      {/* Stop section */}
+      <Box>
+        <Typography variant="h6">
+          <p className="font-medium">Stop From Dhaka</p>
+        </Typography>
+
+        <div className="flex items-center justify-between mt-3 text-gray-400">
+            <div className="flex items-center">
+                <Radio />
+                <label>
+                    <Typography variant="h6">
+                        <p>Non Stop (15)</p>
+                    </Typography>
+                </label>
+            </div>
+            <Typography variant="h6">
+                <p>BDT 3,050</p>
+            </Typography>
+        </div>
+      </Box>
+
+      <div className="my-5 border">
+        <hr />
+      </div>
+
+      {/* Baggage Allowance section */}
+      <Box>
+        <Typography variant="h6">
+          <p className="font-medium">Check in Baggage Allowance</p>
+        </Typography>
+
+        <div className="mt-3 text-gray-400">
+            <div className="flex items-center">
+                <Radio />
+                <label>
+                    <Typography variant="h6">
+                        <p>20 KG (15)</p>
+                    </Typography>
+                </label>
+            </div>
+        </div>
+      </Box>
+
+      <div className="my-5 border">
+        <hr />
+      </div>
+
+      {/* Refund section */}
+      <Box>
+        <Typography variant="h6">
+          <p className="font-medium">Refundable</p>
+        </Typography>
+
+        <div className="mt-3 text-gray-400">
+            <div className="flex items-center">
+                <Radio />
+                <label>
+                    <Typography variant="h6">
+                        <p>Partially Refundable (15)</p>
+                    </Typography>
+                </label>
+            </div>
+        </div>
+      </Box>
+
+      <div className="my-5 border">
+        <hr />
+      </div>
+
+       {/* Departing or arriving section */}
+       <Box className="flex justify-start gap-20 items-center">
+        <div>
+        <Typography variant="h6">
+          <p className="font-medium">Departing from</p>
+        </Typography>
+
+        <div className="mt-3 text-gray-400">
+            <div className="flex items-center">
+                <Radio />
+                <label>
+                    <Typography variant="h6">
+                        <p>DAC</p>
+                    </Typography>
+                </label>
+            </div>
+        </div>
+        </div>
+        <div>
+        <Typography variant="h6">
+          <p className="font-medium">Arriving at</p>
+        </Typography>
+
+        <div className="mt-3 text-gray-400">
+            <div className="flex items-center">
+                <Radio />
+                <label>
+                    <Typography variant="h6">
+                        <p>CGP</p>
+                    </Typography>
+                </label>
+            </div>
+        </div>
+        </div>
       </Box>
     </Box>
   );
